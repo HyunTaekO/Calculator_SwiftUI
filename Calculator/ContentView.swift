475c3c8c7a27431f65a622b19bba69c1175d108b
HyunTaekO <htoh55@naver.com>
2023-12-16 03:43:23 +0900

    refactor: 버튼 스타일 열거형으로 변경 Enum 키워드 활용해서 버튼 스타일 정의 완료

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,15 +7,80 @@
 
 import SwiftUI
 
+enum ButtonType {
+    case one, two, three, four, five, six, seven, eight, nine, zero
+    case clear, not, modular, division, multiplivation, minus, plus, equal, point
+    
+    var displayName: String {
+        switch self {
+        case .one:
+            return "1"
+        case .two:
+            return "2"
+        case .three:
+            return "3"
+        case .four:
+            return "4"
+        case .five:
+            return "5"
+        case .six:
+            return "6"
+        case .seven:
+            return "7"
+        case .eight:
+            return "8"
+        case .nine:
+            return "9"
+        case .zero:
+            return "0"
+        case .clear:
+            return "C"
+        case .not:
+            return "+/-"
+        case .modular:
+            return "%"
+        case .division:
+            return "÷"
+        case .multiplivation:
+            return "×"
+        case .minus:
+            return "−"
+        case .plus:
+            return "+"
+        case .equal:
+            return "="
+        case .point:
+            return "."
+        }
+    }
+    var backgroundColor: Color {
+        switch self {
+        case .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .zero, .point:
+            return .numberButton
+        case .clear, .not, .modular:
+            return .gray
+        default:
+            return .orange
+        }
+    }
+    var foregroundColor: Color {
+        switch self {
+        case .clear, .not, .modular:
+            return .black
+        default:
+            return .white
+        }
+    }
+}
+
 struct ContentView: View {
     
     @State private var answerNumber: String = "0"
-    private let buttonData = [["C", "/", "%", "$"],
-                              ["7", "8", "9", "X"],
-                              ["4", "5", "6", "-"],
-                              ["1", "2", "3", "+"],
-                              ["0", "0", ".", "="]]
-                                                    
+    private let buttonData: [[ButtonType]] = [[.clear, .not, .modular, .division],
+                                              [.seven, .eight, .nine, .multiplivation],
+                                              [.four, .five, .six, .minus],
+                                              [.one, .two, .three, .plus],
+                                              [.zero, .zero, .point, .equal]]
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
@@ -33,17 +98,17 @@ struct ContentView: View {
                         ForEach(line, id: \.self) { item in
                             Button(action: {
                                 if answerNumber == "0" {
-                                    answerNumber = item
+                                    answerNumber = item.displayName
                                 }else {
-                                    answerNumber += item
+                                    answerNumber += item.displayName
                                 }
                             }, label: {
-                                Text(item)
+                                Text(item.displayName)
                                     .frame(width: 80,
                                            height: 80)
-                                    .background(Color("NumberButton"))
+                                    .background(item.backgroundColor)
                                     .clipShape(.circle)
-                                    .foregroundColor(.white)
+                                    .foregroundColor(item.foregroundColor)
                                     .font(.system(size: 33))
                             })
                         }
